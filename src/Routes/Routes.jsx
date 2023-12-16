376e367b448a10db4f29eee419f179ee4089e573
TanimShahriar <ssmtsshr@gmail.com>
2023-12-16 16:33:55 +0600

    sign in and sign up page created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,8 @@ import {
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import ErrorPage from "../Pages/Shared/ErrorPage/ErrorPage";
+import SignUp from "../Pages/Signup/SignUp";
+import SignIn from "../Pages/Signin/SignIn";
 
 
 
@@ -16,7 +18,15 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
-      }
+      },
+      {
+        path: "/signUp",
+        element: <SignUp></SignUp>
+      },
+      {
+        path: "/signIn",
+        element: <SignIn></SignIn>
+      },
 
 
     ]
--- src/main.jsx
+++ src/main.jsx
@@ -12,20 +12,26 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import AuthProvider from './providers/AuthProvider';
 
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
 
-    <QueryClientProvider client={queryClient}>
-      <HelmetProvider>
-        <div className='max-w-screen-2xl mx-auto'>
-          <RouterProvider router={router} />
-        </div>
-      </HelmetProvider>
-    </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <div className='max-w-screen-2xl mx-auto'>
+            <RouterProvider router={router} />
+          </div>
+        </HelmetProvider>
+      </QueryClientProvider>
+
+
+    </AuthProvider>
+
 
   </React.StrictMode>,
 )
